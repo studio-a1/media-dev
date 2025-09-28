47b4a8303737f30c251e5836d35ca3bdc767f325
Julio <you@example.com>
2025-09-28 00:29:58 -0300

    fix: Create 891 valid wrangler.toml for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -24,31 +24,7 @@ const cleanupWranglerJson = () => ({
   },
 });
 
-
-
-
-
-
-
-
-
-
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [cloudflare()],
-});
-
-
-
-
-/*// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-}) */
-
-
+  plugins: [react(), cleanupWranglerJson()],
+})
+;
